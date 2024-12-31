1efff4e1b4e882c991034bf29837d684ed9aeae2
izumi <minyantokunyan@gmail.com>
2024-12-31 15:36:53 +0900

    laravel/UIをインストール　ログインまわりのファイルが自動生成された　npmもインストールなんか知らんけど

--- config/app_org.php
+++ config/app_org.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'ja',
+    'locale' => 'en',
 
     /*
     |--------------------------------------------------------------------------
@@ -175,9 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Debugbar\ServiceProvider::class,
-    
-
     ],
 
     /*
@@ -228,7 +225,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
