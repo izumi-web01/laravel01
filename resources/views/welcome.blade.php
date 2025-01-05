61c95722d5720c0f16bd29a9b0f4ca0e54ad3afd
izumi <minyantokunyan@gmail.com>
2025-01-05 15:07:30 +0900

    config¥app.phpの設定変更（日本語化）

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,6 +73,7 @@
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
+                        
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
